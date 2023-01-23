cfea05c7ed5641af7fb4efcc856cf680411a75b7
Carina Cunha <carina.cunha13@gmail.com>
2023-01-23 15:53:03 -0300

    Add matches test and mock

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,35 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import matchesModel from '../database/models/Matches';
+import { matches, finished} from './mocks/matches.mocks';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Matches route test', () => {
+
+  let chaiHttpResponse: Response;
+
+  afterEach(()=>{
+    sinon.restore
+  });
+
+  it('Shoud list all matches', async()=>{
+    sinon.stub(matchesModel, 'findAll').resolves(matches as any);
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches')
+      .send();
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(matches);
+  })
+});
---
+++ app/backend/src/tests/mocks/matches.mocks.ts
@@ -0,0 +1,30 @@
+export const matches = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Grêmio"
+    }
+  },
+    {
+    "id": 41,
+    "homeTeamId": 16,
+    "homeTeamGoals": 2,
+    "awayTeamId": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Internacional"
+    }
+  },
+]
